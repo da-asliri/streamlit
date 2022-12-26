d39419c6e818ffdf5bb69b88efc83dadffad220a
da-asliri <amin.putera@asliri.id>
2022-12-26 17:23:10 +0700

    test_dev

---
+++ dev_dashboard.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import psycopg2
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+
+
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+
+conn = init_connection()
+
+
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+
+rows = run_query("SELECT * from report_table limit 100;")
