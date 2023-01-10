f8fccde9829f7ac2f1106e754f578c63ea85ce3a
da-asliri <amin.putera@asliri.id>
2023-01-10 14:17:56 +0700

    update gy

--- dev_dashboard.py
+++ dev_dashboard.py
@@ -6,33 +6,34 @@ import psycopg2
 
 def create_connection():
     try:
-        connection = psycopg2.connect(
+        conn = psycopg2.connect(
             host="172.17.84.48",
             user="asliri",
             password="pentaho2022.",
             database="log_db"
         )
-        return connection
+        return conn
     except Exception as e:
         print("Error creating connection:", e)
         return None
 
 
 conn = create_connection()
-cursor = conn.cursor()
 
 if conn:
     print("Connection established successfully!")
 else:
     print("Error establishing connection.")
 
+cursor = conn.cursor()
+
 
 def run_query():
     try:
+
         query = "SELECT * FROM report_table limit 100;"
         cursor.execute(query)
         cursor.close()
-        conn.close()
         print("Query executed successfully.")
     except Exception as e:
         print("Error running query:", e)
